b8a84ffcc11f88997d82c391f38466902312a437
Rafalamao <aaronjt@outlook.com>
2024-12-25 23:24:14 -0600

    feat: :sparkles: First version

---
+++ c #/1_WebPage/Pages/Caca.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace c__.Pages;
+
+public class CacaModel : PageModel
+{
+    private readonly ILogger<CacaModel> _logger;
+
+    public CacaModel(ILogger<CacaModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
