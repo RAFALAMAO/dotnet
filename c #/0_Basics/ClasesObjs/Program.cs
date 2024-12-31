8d060d414fb4a9455fa289aee302b401d5b607a0
Rafalamao <aaronjt@outlook.com>
2024-12-30 23:14:59 -0600

    Se agrega json serialicacion y deserializacion

--- c #/0_Basics/ClasesObjs/Program.cs
+++ c #/0_Basics/ClasesObjs/Program.cs
@@ -1,6 +1,27 @@
-﻿using ClasesObjs.Models;
+﻿using System.Text.Json;
+using ClasesObjs.Models;
 
-Cerveza cerveza = new (200);
+Cerveza cerveza = new (200, "Corona");
 cerveza.Beberse(300);
 
 Console.WriteLine($"{cerveza.CantidadMl} {cerveza.Nombre}");
+
+// Serializacion
+System.Console.WriteLine("Serializacion");
+string cervezaJson = JsonSerializer.Serialize(cerveza);
+Console.WriteLine(cervezaJson);
+
+File.WriteAllText("cerveza.json", cervezaJson);
+
+// Deserializacion
+Console.WriteLine("Deserializacion");
+try
+{
+  string myJson = File.ReadAllText("cerveza.json");
+  Cerveza? cerveza2 = JsonSerializer.Deserialize<Cerveza>(myJson);
+  Console.WriteLine(cerveza2?.Nombre);
+}
+catch (Exception)
+{
+  Console.WriteLine("El archivo no existe");
+}
